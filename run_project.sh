7016e124747e10a0973d555422d3c8a7094c1eda
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-03-31 12:17:08 +0100

    fixed an error with name attribute

--- run_project.sh
+++ run_project.sh
@@ -1,9 +1,8 @@
 COMMAND="./app \
---intervals 3 \
---iterations 7 \
+--intervals 4 \
+--iterations 8 \
 --condition_factor 2 \
---multiplier 1 \
---quantisation_factor 2e-2"
+--multiplier 1"
 
 echo -e "Building docker image\r"
 IMAGE_EXISTS=$(sudo docker inspect cpp_math_problems --format={{.Id}})
@@ -14,11 +13,14 @@ fi
 echo -e "Building and running app..\r"
 CONTAINER_EXISTS=$(sudo docker ps | grep cpp_math_problems)
 if [ -z "$CONTAINER_EXISTS" ]; then
-    sudo docker run -d -v "$PWD":/repo -it cpp_math_problems --name cpp_math_problems
+    sudo docker run -d -v "$PWD":/repo -it cpp_math_problems
 fi
 EMPTY_STR=""
-CONTAINER_ID=$(sudo docker ps --quiet)
+CONTAINER_ID=$(sudo docker ps -q)
+echo -e "$CONTAINER_ID\r"
 
 sudo docker exec "$CONTAINER_ID" bash -c "cd /repo/navier-stokes/build && \
+        rm -rf * *.* && cmake .. && make all && \
         echo -e '\r\n${COMMAND}\r\n' && \
         $COMMAND"
+
